dd9150c87a61be9322592106f778bca397a25385
zi.wang <zi.wang@dixv.onmicrosoft.com>
2020-02-07 10:44:44 +0800

    add show function into spi_led

--- inc/ZSPI_LED.h
+++ inc/ZSPI_LED.h
@@ -11,9 +11,9 @@ namespace codal {
 class ZSPI_LED : public codal::ZSPI {
 public:
     /**
-     * Initialize SPI instance with given pins. But transfer with out dma and do not call schedule
+     * Constructor SPI instance with given pins. But transfer with out dma and do not call schedule
      *
-     * Default setup is 1 MHz, 8 bit, mode 0.
+     * Default setup is 3.2 MHz, 8 bit, mode 0.
      */
     ZSPI_LED(codal::Pin &mosi, codal::Pin &miso, codal::Pin &sclk);
 
@@ -28,7 +28,30 @@ public:
     int startTransfer(const uint8_t *txBuffer, uint32_t txSize, uint8_t *rxBuffer,
                       uint32_t rxSize, PVoidCallback doneHandler, void *arg) override;
 
+    /**
+     * LED use const 3.2 MHz this function is empty
+     * @param frequency
+     * @return
+     */
     int setFrequency(uint32_t frequency) override;
+
+    /**
+     * Light LED by id
+     * @param id which led
+     * @param red 0~255
+     * @param green 0~255
+     * @param blue 0~255
+     * @return error code
+     */
+    int show(int id, uint8_t red, uint8_t green, uint8_t blue);
+
+private:
+    bool firstLight;
+    // LED SPI BUFFER
+    uint8_t buf_bytes[4] = {0x88, 0x8E, 0xE8, 0xEE};
+    uint8_t txBuffer[8 * 3 * 4 + 1] = {};
+    uint32_t txSize = 8 * 3 * 4 + 1;
+    uint8_t mask = 0x03;
 };
 }
 
